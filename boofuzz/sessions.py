2810598859c99359eaedc84cb3542bc0befe2c8b
spwpun <1736090121@qq.com>
2022-03-28 18:06:43 +0800

    use localtime instead of utc-time, which will be used in database filename setting

--- boofuzz/sessions.py
+++ boofuzz/sessions.py
@@ -494,7 +494,8 @@ class Session(pgraph.Graph):
             else:
                 fuzz_loggers = [fuzz_logger_text.FuzzLoggerText()]
 
-        self._run_id = datetime.datetime.utcnow().replace(microsecond=0).isoformat().replace(":", "-")
+        # self._run_id = datetime.datetime.utcnow().replace(microsecond=0).isoformat().replace(":", "-")
+        self._run_id = time.strftime("%Y-%m-%dT%H-%M-%S")
         if db_filename is not None:
             helpers.mkdir_safe(db_filename, file_included=True)
             self._db_filename = db_filename
